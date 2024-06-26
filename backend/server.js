c483b1f54c1db2dc0341ba98c893a270816cefcf
isaacedeg <isaacedeg@gmail.com>
2024-06-26 14:33:49 +0100

    used cors

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import users from './routes/api/users.js';
@@ -8,6 +9,12 @@ import posts from './routes/api/posts.js'
 
 dotenv.config();
 const app = express();
+
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 
 // Init Middleware: to get access to the request body
 app.use(express.json({ extended: false }));
